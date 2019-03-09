db8a403be22f37646e7e9cfd192d260e5106b109
Dawid <dawid.kaleta@interia.pl>
2019-03-09 07:55:21 +0100

    Zmian 09.03

--- Program.cs
+++ Program.cs
@@ -7,18 +7,32 @@ namespace Program1
         static void Main(string[] args)
         {
             // Console.WriteLine("Witaj swiecie!");// formatowanie textu zaznaczamy i ctr K i ctr F
+            JakiMamyDzien();
+            for (int i = 0; i < 2; i++)
+                WprowadzanieDanych();
+        }
+
+        private static void JakiMamyDzien()
+        {
             if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
             { Console.WriteLine(" Dziś jest czwartek"); }
             else
             { Console.WriteLine("Dziś jest inny dzień niż czwartek"); }
-int wiek;
+        }
+
+        private static void WprowadzanieDanych()
+        {
+            Console.Clear();
+            int wiek;
             string imie;
             Console.WriteLine("Jak masz na imię ?");
-                        imie = Console.ReadLine();
+            imie = Console.ReadLine();
             System.Console.WriteLine("Podaj wiek");
-                      wiek = int.Parse(Console.ReadLine());
-            Console.WriteLine("Witaj " + imie + " "+ wiek);
-//testowy 2
+            wiek = int.Parse(Console.ReadLine()); // konwersja inta
+            Console.ForegroundColor = ConsoleColor.Green; //zmiana koloru
+            Console.WriteLine("Witaj " + imie + " " + wiek);
+            Console.ReadKey(); //oczekiwanie na klawisz
+
         }
     }
 }
